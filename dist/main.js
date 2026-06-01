1c155e6de2a33bb967da3ed7446c50cdff8bf7f7
Icha Azizah <icha_azizah_33rpl@student.smktelkom-mlg.sch.id>
2026-06-01 10:32:06 +0700

    menambah swagger

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(process.env.PORT || 3000);
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('API SPP Sekolah')
+        .setDescription('Dokumentasi API untuk sistem pembayaran SPP')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Tambahkan ini
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+
+  // --- TAMBAHKAN BLOK INI ---
+  const config = new DocumentBuilder()
+    .setTitle('API SPP Sekolah')
+    .setDescription('Dokumentasi API untuk sistem pembayaran SPP')
+    .setVersion('1.0')
+    .addBearerAuth() // Jika pakai JWT
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Ini yang bikin path /api jadi ada isinya
+  // --------------------------
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
