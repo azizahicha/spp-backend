70370280e7aba0271bdbd079be547a8945689aef
Icha Azizah <icha_azizah_33rpl@student.smktelkom-mlg.sch.id>
2026-06-01 11:43:14 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Tambahkan ini
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-  // --- TAMBAHKAN BLOK INI ---
   const config = new DocumentBuilder()
-    .setTitle('API SPP Sekolah')
-    .setDescription('Dokumentasi API untuk sistem pembayaran SPP')
+    .setTitle('SPP Backend API')
+    .setDescription('API untuk sistem pembayaran SPP')
     .setVersion('1.0')
-    .addBearerAuth() // Jika pakai JWT
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // Ini yang bikin path /api jadi ada isinya
-  // --------------------------
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
-
-// pancing deploy
